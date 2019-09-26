17b430db9a6abc22b0eab0b4cc97d56f80332a1b
Jon Struve <ocdammo3@gmail.com>
2019-09-26 16:41:08 -0500

    Added jsdocs for v1 js

--- src/models/mongo.js
+++ src/models/mongo.js
@@ -6,6 +6,9 @@ class Model {
     this.schema = schema;
   }
 
+  /**
+   * Creates a schema for the different models.
+   */
   jsonSchema() {
     console.log(typeof this.schema.jsonSchema);
     return typeof this.schema.jsonSchema === 'function'
@@ -13,11 +16,19 @@ class Model {
       : {};
   }
 
+  /**
+   * Gets a given model, either all of that model or a single record of that model selected by id.
+   * @param {number} _id 
+   */
   get(_id) {
     let queryObject = _id ? { _id } : {};
     return this.schema.find(queryObject);
   }
 
+  /**
+   * Creates a new record in a given model.
+   * @param {object} record 
+   */
   create(record) {
     console.log('r',record);
     let newRecord = new this.schema(record);
@@ -25,10 +36,19 @@ class Model {
     return newRecord.save();
   }
 
+  /**
+   * Updates a single record selected by id with whatever record you decide to replace it with.
+   * @param {number} _id 
+   * @param {object} record 
+   */
   update(_id, record) {
     return this.schema.findByIdAndUpdate(_id, record, { new: true });
   }
 
+  /**
+   * Deletes a single record selected by id.
+   * @param {number} _id 
+   */
   delete(_id) {
     return this.schema.findByIdAndDelete(_id);
   }
