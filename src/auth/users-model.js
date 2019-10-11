6fbdf5441ea1ab70332305b464f379381906150b
Jon Struve <ocdammo3@gmail.com>
2019-10-11 10:25:50 -0500

    Added password change capability

--- src/auth/users-model.js
+++ src/auth/users-model.js
@@ -4,6 +4,18 @@ const users = new Mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   email: { type: String },
+  role: {
+    type: String,
+    required: true,
+    default: 'user',
+    enum: ['admin','moderator','user'],
+  },
 });
 
-module.exports = users;
+users.pre('save', async function() {
+  if(this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+})
+
+module.exports = mongoose.model('users', users);
